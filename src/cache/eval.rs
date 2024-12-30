2c730ca36db5cb7e386f6fcc3ff14bd7683c3338
Alexander Durham <alexander.durham@fedex.com>
2024-12-30 13:29:14 -0600

    add nohash hasher

--- src/cache/eval.rs
+++ src/cache/eval.rs
@@ -1,16 +1,15 @@
-use std::{
-    collections::HashMap,
-    sync::{OnceLock, RwLock},
-};
+use std::sync::{OnceLock, RwLock};
 
-static EVAL_CACHE: OnceLock<RwLock<HashMap<u64, i32>>> = OnceLock::new();
+use nohash_hasher::IntMap;
+
+static EVAL_CACHE: OnceLock<RwLock<IntMap<u64, i32>>> = OnceLock::new();
 
 pub struct EvaluationCache {}
 
 impl EvaluationCache {
     pub fn get(zobrist: u64) -> Option<i32> {
         EVAL_CACHE
-            .get_or_init(|| RwLock::new(HashMap::new()))
+            .get_or_init(|| RwLock::new(IntMap::default()))
             .read()
             .expect("failed to read eval cache")
             .get(&zobrist)
@@ -19,7 +18,7 @@ impl EvaluationCache {
 
     pub fn insert(zobrist: u64, eval: i32) {
         EVAL_CACHE
-            .get_or_init(|| RwLock::new(HashMap::new()))
+            .get_or_init(|| RwLock::new(IntMap::default()))
             .write()
             .expect("failed to write eval cache")
             .insert(zobrist, eval);
